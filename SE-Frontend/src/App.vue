49388c81446aa1e795d93d1426a1d310e49fe17a
Jamy Don <mr.jamydon@google.com>
2022-09-26 15:16:25 +0800

    test: test for format

--- SE-Frontend/src/App.vue
+++ SE-Frontend/src/App.vue
@@ -6,19 +6,19 @@
 
 <script>
 import axios from 'axios'
-export default{
-  name:'helloworld',
-  mounted(){
+export default {
+  name: 'helloworld',
+  mounted() {
     axios({
-      method:"get",
-      url:"/api/test/"
-    }).then(res =>{
+      method: "get",
+      url: "/api/test/"
+    }).then(res => {
       this.helloworld = res.data;
     })
   },
-  data(){
-    return{
-      helloworld:"Ajax测试"
+  data() {
+    return {
+      helloworld: "Ajax测试"
     }
   },
 }
