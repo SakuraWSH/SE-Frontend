eccf676418fd12e6c2d38bce5a2603315ae71c20
Jamy Don <mr.jamydon@google.com>
2022-10-04 11:56:25 +0800

    add: login view

--- SE-Frontend/src/views/LoginView.vue
+++ SE-Frontend/src/views/LoginView.vue
@@ -1,26 +1,74 @@
 <template>
-  <main>
-    登录页面
-  </main>
-  <el-row class="mb-4">
-    <el-button>Default</el-button>
-    <el-button type="primary">Primary</el-button>
-    <el-button type="success">Success</el-button>
-    <el-button type="info">Info</el-button>
-    <el-button type="warning">Warning</el-button>
-    <el-button type="danger">Danger</el-button>
-  </el-row>
-  <router-link to="/home">首页</router-link>
-  <router-link to="/about">About</router-link>
+  <el-main>
+    <el-form :model="form" class="login-form">
+      <el-image src="/src/assets/images/logo.png" :class="image"></el-image>
+      <el-form-item label="邮箱">
+        <el-input v-model="form.name" />
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="form.desc" />
+      </el-form-item>
+      <el-form-item class="form-item">
+        <el-button type="primary" round class="button">登录</el-button>
+        <el-button type="primary" round plain class="button">注册</el-button>
+      </el-form-item>
+    </el-form>
+  </el-main>
 </template>
 
+
 <script>
-import { ElButton, ElRow } from 'element-plus';
-import { defineComponent } from 'vue';
+import { ElForm, ElFormItem, ElInput, ElButton, ElImage, ElMain } from 'element-plus';
+import { defineComponent, reactive } from 'vue';
 import '../../node_modules/element-plus/theme-chalk/index.css'
 export default defineComponent({
   components: {
-    ElButton, ElRow,
+    ElForm,
+    ElFormItem,
+    ElInput,
+    ElButton,
+    ElImage,
+    ElMain,
   },
 })
 </script>
+
+
+<script setup>
+const form = reactive({
+  name: '',
+  region: '',
+  date1: '',
+  date2: '',
+  delivery: false,
+  type: [],
+  resource: '',
+  desc: '',
+})
+</script>
+
+
+<style scoped>
+.login-form {
+  justify-content: center;
+  align-content: center;
+  border-radius: 25px;
+  padding: 50px;
+  border: 1px solid;
+}
+
+.image {
+  justify-content: center;
+}
+
+.form-item {
+  border: 1px solid;
+  justify-content: center;
+  align-content: center;
+}
+
+.button {
+  justify-content: center;
+  align-content: center;
+}
+</style>
