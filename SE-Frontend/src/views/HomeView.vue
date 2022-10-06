26fc3d8fa37e4a026a1fd95f1c8f56fd9780ef89
Jamy Don <mr.jamydon@google.com>
2022-10-06 13:49:34 +0800

    add: elements in homeview

--- SE-Frontend/src/views/HomeView.vue
+++ SE-Frontend/src/views/HomeView.vue
@@ -1,5 +1,22 @@
 <template>
-  <main>
-    首页
-  </main>
+  <el-row justify="center">
+    <el-col span="1" v-for="n in 12">
+      <el-card>
+        111
+      </el-card>
+    </el-col>
+  </el-row>
 </template>
+
+<script>
+import { ElCard, ElRow, ElCol } from 'element-plus';
+import { defineComponent, reactive } from 'vue';
+import '../../node_modules/element-plus/theme-chalk/index.css'
+export default defineComponent({
+  components: {
+    ElCard,
+    ElRow,
+    ElCol,
+  },
+})
+</script>
