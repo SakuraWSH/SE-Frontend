3f376088f34987b0bc5311ca273044bb698576b2
Sakura <2000013098@stu.pku.edu.cn>
2022-10-15 22:00:56 +0800

    Detail

--- src/views/modules/DetailView.vue
+++ src/views/modules/DetailView.vue
@@ -1,5 +1,131 @@
 <template>
-  <main>
-    详情页面
-  </main>
+  <el-container>
+    <el-header>
+      <NavBar />
+    </el-header>
+    <div class = "main">
+      <div class = "swiper">
+        <div class = "imgpage">
+          <img :src = "imgList[currentIndex]" class="img">
+        </div>
+        <div class="page" v-if="this.imgList.length > 1">
+          <ul class = "daohang">
+            <li @click="gotoPage(prevIndex)">&lt;</li>
+            <li v-for="(item,index) in imgList" @click="gotoPage(index)" :class="{'current':currentIndex == index}">  {{index+1}}</li>
+            <li @click="gotoPage(nextIndex)">&gt;</li>
+          </ul>
+        </div>
+      </div>
+      <div class = "rightdetail">
+        <div class = "title">标题</div>
+        <div class = "ps">备注</div>
+        <div class = "price">价格</div>
+        <div class = "other">购买 联系对方 举报</div>
+      </div>
+    </div>
+    <div class = "comment">
+      评论
+    </div>
+  </el-container>
+  
 </template>
+
+<script setup>
+import NavBar from "../../components/NavBar.vue"
+</script>
+
+<script>
+import { ElHeader,ElContainer,ElAside } from 'element-plus';
+import {defineComponent,reactive} from 'vue';
+import '../../../node_modules/element-plus/theme-chalk/index.css'
+export default defineComponent({
+  components:{
+    ElHeader,
+    ElContainer,
+    ElAside,
+  },
+  
+  data(){
+    return {
+      imgList:["https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png","https://img1.baidu.com/it/u=691823892,2308173677&fm=253&fmt=auto&app=138&f=JPEG?w=861&h=500"],
+      currentIndex: 0,
+    } 
+  },
+  methods: {
+    gotoPage(index) {
+      this.currentIndex = index;
+    }
+  },
+  computed: {
+  //上一张
+    prevIndex() {
+      if(this.currentIndex == 0) {
+        return this.dataList.length - 1;
+      }else{
+        return this.currentIndex - 1;
+      }
+    },
+  //下一张
+    nextIndex() {
+      if(this.currentIndex == this.dataList.length - 1) {
+        return 0;
+      }else {
+        return this.currentIndex + 1;
+      }
+    }
+}
+})
+</script>
+
+<style scoped>
+.el-container{
+  height: 100%;
+  width: 100%;
+}
+.el-header{
+  padding: 0;
+}
+.main{
+  height: 70%;
+  width: 100%;
+  display: flex;
+}
+.swiper{
+  height: 100%;
+  width: 50%;
+}
+.img{
+  height: 100%;
+  width: 100%;
+}
+.imgpage{
+  height: 90%;
+  width: 100%;
+}
+.page {
+    background: palegreen;
+    padding-top: 0;
+    padding: 0;
+    height: 10%;
+    width: 100%;
+}
+ul li {
+    list-style: none;
+    float: left;
+    width: 20%;
+    height: 50%;
+    line-height: 40px;
+    text-align: center;
+    cursor: pointer;
+    color: palegoldenrod;
+    font-size: 14px;
+    background-color: aquamarine;
+}
+.current {
+    color: orangered;
+}
+.rightdetail{
+  height: 100%;
+  width: 50%;
+}
+</style>
--- src/views/modules/ListView.vue
+++ src/views/modules/ListView.vue
@@ -1,42 +1,182 @@
 <template>
-  <div class="tcm">
-    <div class="margin">让我看看这个东西在哪里</div>
-    <div class="margin1">让我看看这个东西在哪里1</div>
+  <div class = "father">
+    <el-row class="sonthree">
+      <el-row class="grandsonone" justify="center">
+        <el-col :span="4" >
+          <el-card shadow="hover" @click="goto('/detail')">
+            <img
+              src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+              class="image"
+            />
+          </el-card>
+        </el-col>
+        <el-col :span="4">
+          <el-card shadow="hover" @click="goto('/home')">
+            <img
+              src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+              class="image"
+            />
+          </el-card>
+        </el-col>
+        <el-col :span="4">
+          <el-card shadow="hover" @click="goto('/home')">
+            <img
+              src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+              class="image"
+            />
+          </el-card>
+        </el-col>
+        <el-col :span="4">
+          <el-card shadow="hover" @click="goto('/home')">
+            <img
+              src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+              class="image"
+            />
+          </el-card>
+        </el-col>
+        <el-col :span="4">
+          <el-card shadow="hover" @click="goto('/home')">
+            <img
+              src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+              class="image"
+            />
+          </el-card>
+        </el-col>
+      </el-row>
+      <el-row class="grandsontwo" justify="center">
+        <el-col :span="4" >
+          <el-card shadow="hover" @click="goto('/home')">
+            <img
+              src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+              class="image"
+            />
+          </el-card>
+        </el-col>
+        <el-col :span="4">
+          <el-card shadow="hover" @click="goto('/home')">
+            <img
+              src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+              class="image7"
+            />
+          </el-card>
+        </el-col>
+        <el-col :span="4">
+          <el-card shadow="hover" @click="goto('/home')">
+            <img
+              src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+              class="image"
+            />
+          </el-card>
+        </el-col>
+        <el-col :span="4">
+          <el-card shadow="hover" @click="goto('/home')">
+            <img
+              src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+              class="image"
+            />
+          </el-card>
+        </el-col>
+        <el-col :span="4">
+          <el-card shadow="hover" @click="goto('/home')">
+            <img
+              src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+              class="image"
+            />
+          </el-card>
+        </el-col>
+      </el-row>
+      <el-row class="grandsonthree" justify="center">
+        <el-col :span="4" >
+          <el-card shadow="hover" @click="goto('/home')">
+            <img
+              src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+              class="image"
+            />
+          </el-card>
+        </el-col>
+        <el-col :span="4">
+          <el-card shadow="hover" @click="goto('https://pkuhelper.pku.edu.cn/hole/')">
+            <img
+              src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+              class="image"
+            />
+          </el-card>
+        </el-col>
+        <el-col :span="4">
+          <el-card shadow="hover" @click="goto('/home')">
+            <img
+              src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+              class="image"
+            />
+          </el-card>
+        </el-col>
+        <el-col :span="4">
+          <el-card shadow="hover" @click="goto('/home')">
+            <img
+              src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+              class="image"
+            />
+          </el-card>
+        </el-col>
+        <el-col :span="4">
+          <el-card shadow="hover" @click="jump">
+            <img
+              src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+              class="image"
+            />
+          </el-card>
+        </el-col>
+      </el-row>
+    </el-row>
   </div>
 </template>
 <script>
-  
+import { ElRow,ElCol,ElCard,ElDivider,ElButton } from 'element-plus';
+import { defineComponent, reactive } from 'vue';
+import '../../../node_modules/element-plus/theme-chalk/index.css'
+export default defineComponent({
+  components: {
+    ElCol,
+    ElRow,
+    ElCard,
+    ElDivider,
+    ElButton,
+  },
+  data () {
+    return {}
+  },
+  methods: {
+    goto(router) {
+      this.$router.replace(router);
+    }
+  }
+})
 </script>
 
-<style>
-.tcm{
-  margin-top: 0px;
-  margin-left: 0px;
-  margin-bottom: 0px;
-  margin-right: 0px;
-  padding-left: 0px;
-  padding-top: 0px;
-  padding-bottom: 0px;
-  padding-right: 0px;
-  background-color: blue;
-  height: 1000px;
-  width: 1920px;
-}
-.margin{
-  margin-top: 0px;
-  float: left;
-  top:0px;
-  left: 0px;
-  right: 0px;
-  color: aquamarine;
-  background-color: antiquewhite;
-}
-.margin1{
-  border-radius: 500px;
-  padding: 50px;
-  top:50px;
-  left: 50px;
-  color: skyblue;
-  background-color: brown;
-}
+<style scoped>
+  .father{
+    height: 100%;
+    width: 100%;
+  }
+  .sonthree{
+    height: 70%;
+    top: 25%;
+    width: 100%;
+  }
+  .grandsonone{
+    height:33%;
+    width: 100%;
+  }
+  .grandsontwo{
+    height:33%;
+    width: 100%;
+  }
+  .grandsonthree{
+    height:33%;
+    width: 100%;
+  }
+  .image{
+    height: 100%;
+    width: 100%;
+  }
 </style>
