a84698508f3b1334dd9098a37d12a32c173f8c90
Jamy Don <1823376557@qq.com>
2022-10-25 12:49:44 +0800

    add: test for upload img von base64

--- src/test/test.vue
+++ src/test/test.vue
@@ -1,22 +1,28 @@
 <template>
     <h1>Test</h1>
     <input type="file" accept="image/jpeg/*" @change="uploadImage()" />
+    <img v-if="flag == '1'" :src="rawImg" alt="Error">
+
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            rawImg: "",
+            flag: '0',
+        }
+    },
     methods: {
         uploadImage() {
             const file = document.querySelector('input[type=file]').files[0]
             const reader = new FileReader()
 
-            let rawImg;
             reader.onloadend = () => {
-                rawImg = reader.result;
-                console.log(rawImg);
+                this.rawImg = reader.result;
             }
             reader.readAsDataURL(file);
-            console.log(file)
+            this.flag = '1'
         }
     }
 }
