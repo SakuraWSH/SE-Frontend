76889f694fce51bc6019b532918e5435b70baddb
Jamy Don <1823376557@qq.com>
2022-10-25 11:42:16 +0800

    add: test

---
+++ src/test/test.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1>Test</h1>
+    <input type="file" accept="image/jpeg/*" @change="uploadImage()" />
+</template>
+
+<script>
+export default {
+    methods: {
+        uploadImage() {
+            const file = document.querySelector('input[type=file]').files[0]
+            const reader = new FileReader()
+
+            let rawImg;
+            reader.onloadend = () => {
+                rawImg = reader.result;
+                console.log(rawImg);
+            }
+            reader.readAsDataURL(file);
+            console.log(file)
+        }
+    }
+}
+</script>
