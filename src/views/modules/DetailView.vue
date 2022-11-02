aefd9d312d82c9bea4a586579d870c0afc3ad44b
wsf <2000013176@stu.pku.edu.cn>
2022-11-02 20:16:31 +0800

    change some

--- src/views/modules/DetailView.vue
+++ src/views/modules/DetailView.vue
@@ -3,31 +3,42 @@
     <el-header>
       <NavBar />
     </el-header>
-    <div class = "main">
-      <div class = "swiper">
-        <div class = "imgpage">
-          <img :src = "imgList[currentIndex]" class="img">
-        </div>
-        <div class="page" v-if="this.imgList.length > 1">
-          <ul class = "daohang">
-            <li @click="gotoPage(prevIndex)">&lt;</li>
-            <li v-for="(item,index) in imgList" @click="gotoPage(index)" :class="{'current':currentIndex == index}">  {{index+1}}</li>
-            <li @click="gotoPage(nextIndex)">&gt;</li>
-          </ul>
+    <div class="main">
+      <div class="swiper">
+        <div class="block">
+          <el-carousel>
+            <el-carousel-item v-for="item in 4" :key="item">
+              <h3>{{ item }}</h3>
+            </el-carousel-item>
+          </el-carousel>
         </div>
       </div>
-      <div class = "rightdetail">
-        <div class = "title">标题</div>
-        <div class = "ps">备注</div>
-        <div class = "price">价格</div>
-        <div class = "other">购买 联系对方 举报</div>
+      <div class="rightdetail">
+        <div class="title">这里是标题</div>
+        <el-divider></el-divider>
+        <div class="ps">
+          这里是描述<br />
+          这里是描述<br />
+          这里是描述<br />
+          这里是描述<br />
+          这里是描述<br />
+          这里是描述<br />
+          这里是描述<br />
+          这里是描述<br />
+          这里是描述</div>
+        <div class="price">￥114.514</div>
+        <el-row>
+          <el-button shadow="hover" type="buy" round>购买</el-button>
+          <el-button shadow="hover" type="contact" round>联系发布者</el-button>
+          <el-button shadow="hover" type="report" round>举报</el-button>
+        </el-row>
       </div>
     </div>
-    <div class = "comment">
+    <div class="comment">
       评论
     </div>
   </el-container>
-  
+
 </template>
 
 <script setup>
@@ -35,21 +46,27 @@ import NavBar from "../../components/NavBar.vue"
 </script>
 
 <script>
-import { ElHeader,ElContainer,ElAside } from 'element-plus';
-import {defineComponent,reactive} from 'vue';
+import { ElHeader, ElContainer, ElAside, ElButton, ElCarousel, ElCarouselItem, ElDivider, ElCol, ElRow } from 'element-plus';
+import { defineComponent, reactive } from 'vue';
 import '../../../node_modules/element-plus/theme-chalk/index.css'
 export default defineComponent({
-  components:{
+  components: {
     ElHeader,
     ElContainer,
     ElAside,
+    ElButton,
+    ElCarousel,
+    ElCarouselItem,
+    ElDivider,
+    ElCol,
+    ElRow,
   },
-  
-  data(){
+
+  data() {
     return {
-      imgList:["https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png","https://img1.baidu.com/it/u=691823892,2308173677&fm=253&fmt=auto&app=138&f=JPEG?w=861&h=500"],
+      imgList: ["https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png", "https://img1.baidu.com/it/u=691823892,2308173677&fm=253&fmt=auto&app=138&f=JPEG?w=861&h=500"],
       currentIndex: 0,
-    } 
+    }
   },
   methods: {
     gotoPage(index) {
@@ -57,75 +74,120 @@ export default defineComponent({
     }
   },
   computed: {
-  //上一张
+    //上一张
     prevIndex() {
-      if(this.currentIndex == 0) {
+      if (this.currentIndex == 0) {
         return this.dataList.length - 1;
-      }else{
+      } else {
         return this.currentIndex - 1;
       }
     },
-  //下一张
+    //下一张
     nextIndex() {
-      if(this.currentIndex == this.dataList.length - 1) {
+      if (this.currentIndex == this.dataList.length - 1) {
         return 0;
-      }else {
+      } else {
         return this.currentIndex + 1;
       }
     }
-}
+  }
 })
 </script>
 
 <style scoped>
-.el-container{
+.el-container {
   height: 100%;
   width: 100%;
 }
-.el-header{
+
+.el-header {
   padding: 0;
 }
-.main{
-  height: 70%;
+
+.main {
+  height: 60%;
   width: 100%;
   display: flex;
 }
-.swiper{
+
+.swiper {
   height: 100%;
   width: 50%;
 }
-.img{
-  height: 100%;
-  width: 100%;
+
+.block {
+  height: 80%;
+  width: 60%;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
 }
-.imgpage{
-  height: 90%;
-  width: 100%;
-}
-.page {
-  background: palegreen;
-  padding-top: 0;
-  padding: 0;
-  height: 10%;
-  width: 100%;
-}
-ul li {
-  list-style: none;
-  float: left;
-  width: 20%;
+
+.rightdetail {
   height: 50%;
-  line-height: 40px;
-  text-align: center;
-  cursor: pointer;
-  color: palegoldenrod;
-  font-size: 14px;
-  background-color: aquamarine;
-}
-.current {
-    color: orangered;
-}
-.rightdetail{
-  height: 100%;
   width: 50%;
+}
+
+.title {
+  font-size: 50px;
+  font-family: "黑体";
+
+}
+
+.ps {
+  font-size: 18px;
+  font-family: "宋体";
+}
+
+.price {
+  font-size: 40px;
+  color: red;
+  font-family: 'Times New Roman', Times, serif;
+}
+
+.el-button--buy {
+  font-size: 15px;
+  font-family: "黑体";
+  background-color: red;
+  color: white;
+}
+
+.el-button--contact {
+  font-size: 15px;
+  font-family: "黑体";
+  background-color: lightgray;
+}
+
+.el-button--report {
+  font-size: 15px;
+  font-family: "黑体";
+  background-color: red;
+  color: white;
+}
+
+.comment {
+  font-family: "宋体";
+  font-size: 45px;
+}
+
+.el-carousel__item h3 {
+  color: #475669;
+  font-size: 14px;
+  opacity: 0.75;
+  line-height: 150px;
+  margin: 0;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n+1) {
+  background-color: #d3dce6;
+}
+
+.el-divider{
+  margin: 0 0;
+  background: 0 0;
 }
 </style>
